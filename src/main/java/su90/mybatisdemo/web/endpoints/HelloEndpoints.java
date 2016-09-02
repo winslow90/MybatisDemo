e436f77aa05c03c54283e7ac83ad19f6fc512b7b
superman90 <liwentao90@yahoo.com>
2016-09-02 15:58:18 -0400

    modify the helloendpoints swagger2 annot config

--- src/main/java/su90/mybatisdemo/web/endpoints/HelloEndpoints.java
+++ src/main/java/su90/mybatisdemo/web/endpoints/HelloEndpoints.java
@@ -5,6 +5,12 @@
  */
 package su90.mybatisdemo.web.endpoints;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/hello")
 public class HelloEndpoints {
     
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ApiOperation(value = "GETGREETING", nickname = "getGreeting")
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET,produces = MediaType.TEXT_PLAIN_VALUE)
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue="winslow")
+//      })
+    @ApiResponses(value = { 
+            @ApiResponse(code = 200, message = "Success", response = String.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")}) 
+    
     public String sayHellow(@PathVariable String name){
         return "Hello"+name+"~";
     }
